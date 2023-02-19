d8d2ea936a2af8c30c7f51585e4485c40f6492be
Jiazhe Chen <chenjiazhe@outlook.com>
2023-02-19 09:45:23 -0500

    A5 labs 2.24

--- public/labs/a5/todos/index.js
+++ public/labs/a5/todos/index.js
@@ -1,1 +1,8 @@
-alert('Hello World!');
+import TodoList from "./TodoList.js";
+// alert('Hello World!');
+$('#wd-todo').append(`
+   <div class="container">
+       <h1>Todo example</h1>
+       ${TodoList()}
+   </div>
+`);
